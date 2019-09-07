fc4fc52f1ff81c2efff44cc453d155342261e499
ChouhanParul <parulchouhan1990@gmail.com>
2019-09-07 22:05:51 +0530

    error pages

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from app import app, db
 from app.forms import RegistrationForm
 from app.models import Employee
 
+#route for the home page
 @app.route('/')
 @app.route('/home')
 def home():
@@ -11,6 +12,7 @@ def home():
         employee = Employee.query.filter_by(is_deleted='0')
     return render_template('home.html', posts = employee)
 
+#route for delete the employee
 @app.route('/deleteEmployee', methods=['GET', 'POST'])
 def deleteEmployee():
     if request.method == 'POST':
@@ -20,7 +22,7 @@ def deleteEmployee():
         db.session.commit()
         return '1'
         
-
+#route for add the employee
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
@@ -35,3 +37,13 @@ def register():
         else:
             flash('Somthing went wrong', 'danger')
     return render_template('register.html', title='Register', form=form)
+
+#route for 404 page
+@app.errorhandler(404)
+def error_404(error):
+    return render_template('errors/404.html'), 404
+
+#route for 500 page
+@app.errorhandler(500)
+def error_500(error):
+    return render_template('errors/500.html'), 404
